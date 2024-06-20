704510408dbf4d823e120e133ffae9eff4382fec
YTDavid93 <ytd3809@gmail.com>
2024-06-20 17:30:38 +0545

    visualize bubblesort

--- app/BubbleSort.js
+++ app/BubbleSort.js
@@ -13,14 +13,13 @@ const bubbleSort = (arr) => {
   const n = arr.length;
 
   for (let i = 0; i < n; i++) {
-    console.log("In i loop");
     for (j = 0; j < n - i - 1; j++) {
       console.log("In j loop");
       if (arr[j] > arr[j + 1]) {
         //swap the values
         swap(arr, j, j + 1);
         // Record the step
-        steps.push([...arr]); // Push the copy of the array
+        steps.push({ array: [...arr], compared: [j, j + 1] }); // Push the copy of the array
       }
     }
   }
@@ -31,16 +30,26 @@ const visualizeBubbleSort = async (array, bars, delay) => {
   const steps = bubbleSort(array);
 
   for (const step of steps) {
+    const { array: comparedArray, compared } = step;
+
     for (let i = 0; i < bars.length; i++) {
-      bars[i].style.height = `${step[i] * 3}px`;
-      bars[i].style.background = "blue";
+      if (bars[i].style.background !== "green") {
+        bars[i].style.background = "cyan";
+      }
+
+      // in comparison color changes
+      if (i === compared[0] || i === compared[1]) {
+        bars[i].style.background = "blue";
+      }
+
+      // update bar height
+      bars[i].style.height = `${comparedArray[i] * 3}px`;
     }
     await sleep(delay);
   }
 
-  // after storing is complete, set all bars to green
+  // after all the sorting is done change it to green
   for (let i = 0; i < bars.length; i++) {
     bars[i].style.background = "green";
   }
 };
-
