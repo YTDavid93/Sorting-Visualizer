30dbcd6656e8eb767a1b22682a84a75ca2b43c16
YTDavid93 <ytd3809@gmail.com>
2024-06-20 17:30:51 +0545

    visualize insertionsort

--- app/InsertionSort.js
+++ app/InsertionSort.js
@@ -5,12 +5,19 @@ const insertionSort = (arr) => {
   for (let i = 1; i < n; i++) {
     let key = arr[i];
     let j = i - 1;
+
     while (j >= 0 && arr[j] > key) {
+      // compare
       arr[j + 1] = arr[j];
-      j = j - 1; // to exit from the while loop
+      // swaps takes place
+      steps.push({array: [...arr], comparison: [j+1, j]}); // for visualization comparison takes place
+      j--; // to exit from the while loop
     }
+    
+    // place the key in current positions
     arr[j + 1] = key;
-    steps.push([...arr]);
+    // record the swap steps
+    steps.push({array: [...arr], comparison: null, swap: [j+1, i]}); // for visualization swap takes place
   }
   return steps;
 };
@@ -19,9 +26,26 @@ const visualizeInsertionSort = async (arr, bars, delay) => {
   const steps = insertionSort(arr);
 
   for (const step of steps) {
+    const { array: insertionSortArray, comparison, swap} = step;
+    console.log(insertionSortArray)
     for (let i = 0; i < bars.length; i++) {
-      bars[i].style.height = `${step[i] * 3}px`;
-      bars[i].style.background = "blue";
+
+      // reset all the bars of default color 
+      bars[i].style.background = "cyan";
+
+      if (comparison && comparison.includes(i))
+        {
+          bars[i].style.background = "blue";
+          console.log("comparison takes place")
+        }
+
+      if (swap && swap.includes(i))
+        {
+          bars[i].style.background = "yellow";
+          console.log("swap takes place")
+        }
+
+      bars[i].style.height = `${insertionSortArray[i] * 3}px`;
     }
     await sleep(delay);
   }
